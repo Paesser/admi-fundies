5eeaac7fff4c4729563c5c8d6fbbe042bf051e03
Tevin <motevin@gmail.com>
2018-02-14 13:10:13 +0300

    Hw1 q5 skeleton

---
+++ HW1Question5.java
@@ -0,0 +1,9 @@
+class HW1Question5 {
+	public static void main(String[] args) {
+		String param1 = args[0];
+		String param2 = args[1];
+		String param3 = args[2];
+
+		// print true if all params are the same, false otherwise
+	}
+}
