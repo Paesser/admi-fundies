4e7777a2d3782847fc3b7eeeb29999f1ca691834
Tevin <motevin@gmail.com>
2018-03-28 11:50:06 +0300

    twenty questions

---
+++ recursion/TwentyQuestions.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TwentyQuestions {
+    static Scanner sc = new Scanner(System.in);
+
+        // You can read user input as follows:
+        // boolean answer = sc.nextBoolean();
+        public static int search(int min, int max) {
+            // Base Case - when min and max are the same integer
+            if (/* */) {
+                /* */
+            } else {
+                System.out.println("Is your number less than " + max);
+                boolean lessThan = sc.nextBoolean();
+
+                if (lessThan) {
+                    /* */
+                } else {
+                    /* */
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            int min = 0;
+            int max = 100;
+            System.out.println("Think of a number between " + min + " and " + max + ".\nPress Enter to Continue");
+            sc.next();
+            int answer = search(min, max);
+
+
+            System.out.println("Your number is " + answer);
+        }
+}
